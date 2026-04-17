99e280825fa314263d1bd9430d4d1d327c844ed5
Nakedbeanso <parvizal456@gmail.com>
2026-04-17 11:20:37 +0700

    third commit

--- Inv-Management/app/Http/Controllers/AuthController.php
+++ Inv-Management/app/Http/Controllers/AuthController.php
@@ -6,5 +6,38 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register (Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:255|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8|confirmed'
+        ]);
+
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password)
+    ]);
+
+    public function login (Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = USer::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check ($request->password, $user->password)){
+            throw ValidationException::withMessages([
+                'email' => ['incorect'],
+
+            ])
+        }
+
+    }
+
+    
+    }
 }
